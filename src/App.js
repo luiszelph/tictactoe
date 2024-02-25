730b51774e860419886fe0c1fc865926787bdedf
Zelph Moreno <luiszelph.morenoruiz@gmail.com>
2024-02-25 16:19:05 -0600

    Se intenta agregar responsividad

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import './App.css';
 import TicTacToe from './Components/TicTacToe/Tictactoe';
 
 function App() {
   return (
-    <div>
-      <TicTacToe/>
-    </div>
+    <TicTacToe/>
   );
 }
 
